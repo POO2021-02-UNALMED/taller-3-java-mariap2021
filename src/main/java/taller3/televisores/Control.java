0220df718980f75d1269c5dda65ffae1eec323c0
mariap2021 <92173632+mariap2021@users.noreply.github.com>
2021-10-29 22:04:51 -0500

    Add files via upload

---
+++ src/main/java/taller3/televisores/Control.java
@@ -0,0 +1,81 @@
+package taller3.televisores;
+
+public class Control {
+	private TV tv;
+	
+	public void enlazar(TV tv) {
+		this.tv = tv;
+		this.tv.control = this;
+		
+		
+	}
+	
+	
+	
+	public TV getTv() {
+		return tv;
+	}
+
+
+
+	public void setTv(TV tv) {
+		this.tv = tv;
+	}
+
+
+
+	public void turnOn() {
+		this.tv.turnOn();
+	}
+
+	public void turnOff() {
+		this.tv.turnOff();
+	}
+	
+	
+	public void canalUp() {
+		if ( tv.estado== false|| tv.canal==120) {
+			return;
+		}
+		else {
+			tv.canal ++;
+		}
+	}
+	public void canalDown() {
+		if ( tv.estado== false|| tv.canal==0) {
+			return;
+		}
+		else {
+			tv.canal ++;
+		}
+	}
+	
+	
+	public void volumenDown() {
+		if ( tv.estado == false || tv.volumen==0) {
+			return;
+		}
+		else {
+			tv.volumen --;
+		}
+	}
+	
+	
+	public void volumenUp() {
+		if ( tv.estado== false|| tv.volumen==7) {
+			return;
+		}
+		else {
+			tv.volumen ++;
+		}
+	}
+	
+	
+	public void setCanal(int canal) {
+		if(tv.estado|| canal >=1 || canal<=120) {
+			this.tv.canal=canal;
+		}
+	}
+	
+	
+}
